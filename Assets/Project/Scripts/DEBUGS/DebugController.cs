35cc08365cb25e55ff5f8bc67356a5fc4802aa56
ykNe0372 <feng99748@gmail.com>
2026-05-12 12:03:04 +0900

    【変更】旧名称だったのを変更

--- Assets/Project/Scripts/DEBUGS/DebugController.cs
+++ Assets/Project/Scripts/DEBUGS/DebugController.cs
@@ -5,14 +5,14 @@ using System.Collections.Generic;
 public class DebugController : MonoBehaviour {
     [SerializeField] private bool _isDebugMode = true;
     [SerializeField] private GameObject _debugPanel;
-    [SerializeField] private TypingManager _typingMg;
+    [SerializeField] private TypingManager _typingManager;
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.F12)) _debugPanel.SetActive(!_debugPanel.activeSelf);
         if (!_isDebugMode) return;
     }
 
-    public void Debug_NextQuestion() => _typingMg.Debug_NextQuestion();
-    public void Debug_ForceBonus() => _typingMg.Debug_ForceBonus();
-    public void Debug_ToggleRuby() => _typingMg.Debug_ToggleRuby();
+    public void Debug_NextQuestion() => _typingManager.Debug_NextQuestion();
+    public void Debug_ForceBonus() => _typingManager.Debug_ForceBonus();
+    public void Debug_ToggleRuby() => _typingManager.Debug_ToggleRuby();
 }
